5dd852846b1668c1c66033bf98fc46b3d5e184d6
Dean <gao.dean@hotmail.com>
2022-06-04 20:38:00 +1000

    change deep sleep settings in cradio conf

--- config/34keys.h
+++ config/34keys.h
@@ -11,5 +11,4 @@
 #define MO(LAYER) &mo_tap LAYER F24
 
 // Keep sticky keys active for a long time so they effectively do not time out
-#define STICKY_KEY_TIMEOUT=6000
-#define CONFIG_ZMK_SLEEP=y
+#define STICKY_KEY_TIMEOUT 6000
