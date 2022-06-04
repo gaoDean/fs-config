b9b16cffe0bff93ec133961c2e9e227ee3d6598a
Dean <gao.dean@hotmail.com>
2022-06-04 20:29:08 +1000

    add deep sleep mode and keep times default

--- config/34keys.h
+++ config/34keys.h
@@ -11,4 +11,5 @@
 #define MO(LAYER) &mo_tap LAYER F24
 
 // Keep sticky keys active for a long time so they effectively do not time out
-#define STICKY_KEY_TIMEOUT 60000
+#define STICKY_KEY_TIMEOUT=6000
+#define CONFIG_ZMK_SLEEP=y
