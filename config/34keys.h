f1354aa960d82ed075d8cb926c00b336c1df4a4e
Dean <gao.dean@hotmail.com>
2022-06-12 09:53:40 +1000

    cleanup keymap files

--- config/34keys.h
+++ config/34keys.h
@@ -6,7 +6,7 @@
 #define FUNC 2
 #define GAME 3
 
-#define SK(KEY) &nk KEY KEY
+// #define SK(KEY) &nk KEY KEY
 #define MO(LAYER) &mo_tap LAYER F24
 
 // Keep sticky keys active for a long time so they effectively do not time out
